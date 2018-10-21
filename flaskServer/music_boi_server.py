61ad6902f98dda0fddf91ad5d3b62c66c51101b3
HalmonLui <halmonlui@gmail.com>
2018-10-21 03:52:15 +0100

    Mostly finished app, changed server a bit

--- flaskServer/music_boi_server.py
+++ flaskServer/music_boi_server.py
@@ -1,10 +1,13 @@
 #Imports
 from flask import Flask, request
+from flask_cors import CORS
+import json
 #from text_emotion_analyzer.py import emotion_analyzer
 #from music_boi.py import music_boi
 
 #Set app for Flask
 app = Flask(__name__)
+CORS(app)
 
 #Routes
 @app.route('/')
@@ -17,5 +20,7 @@ def submitString():
     text = request.form['text']
     #emotion = emotion_analyzer(text)
     #midi = music_boi(emotion)
+    cool = {"text": "GOTEM! " + text}
+    cool = json.dumps(cool)
     print("Received: " + text)
-    return text
+    return cool
--- musicBoiApp/App.js
+++ musicBoiApp/App.js
@@ -5,26 +5,30 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ""
+      text: "Paste text or type text here!",
+      test: "lol",
     };
   }
 
   submitText() {
-    var newHP = {'currentHP':hp};
+    this.textInput.clear();
+    let text = this.state.text;
     fetch('http://musicbois.serveo.net/submitString', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/x-www-form-urlencoded'
       },
-      body: JSON.stringify(newHP)
+      body: '&text='+text
     })
       .then(res => res.json())
       .then((resp) => {
+        this.setState({test: resp.text});
       })
   }
 
 
   render() {
+    let test = this.state.test;
     return (
       <ImageBackground
         source={{uri: 'https://thumbs.gfycat.com/DisguisedTangibleArmyant-size_restricted.gif'}}
@@ -32,20 +36,22 @@ export default class App extends React.Component {
       >
         <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
           <StatusBar hidden/>
-          <Image source={{uri: 'https://raw.githubusercontent.com/sonamghosh/hack_harvard_2018/FlaskServer/musicBoiLogo/musicBoiLogo3.png'}}
+          <Image source={{uri: 'https://raw.githubusercontent.com/sonamghosh/hack_harvard_2018/FlaskServer/musicBoiLogo/musicBoiLogo5.png'}}
                 style={{width: 139, height: 85}}/>
-          <Text>Transform words into music</Text>
+          <Text style={styles.text}>Transform words into music</Text>
+          <Text style={styles.text}>{test}</Text>
           <TextInput
             style={styles.input}
             placeholder="Paste text or type text here!"
             multiline={true}
+            ref={input => { this.textInput = input }}
             onChangeText={(text) => this.setState({text})}
           />
           <TouchableOpacity
-            onPress={this.submitText}
+            onPress={this.submitText.bind(this)}
           >
             <View style={styles.submit}>
-              <Text>Create</Text>
+              <Text style={styles.text}>Create</Text>
             </View>
           </TouchableOpacity>
         </KeyboardAvoidingView>
@@ -58,13 +64,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-
+    color: '#87ceeb',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color: '#87ceeb',
+  },
   input: {
     width: "80%",
-    height: '40%',
+    height: '20%',
     borderRadius: 2,
     padding: 5,
     margin: 20,
@@ -82,9 +91,9 @@ const styles = StyleSheet.create({
     margin: 10,
     justifyContent: 'center',
     alignItems: 'center',
-
+    color: '#87ceeb',
     opacity: 0.8,
     borderColor: 'skyblue',
-    borderWidth: 0.5,
+    borderWidth: 1,
   },
 });
