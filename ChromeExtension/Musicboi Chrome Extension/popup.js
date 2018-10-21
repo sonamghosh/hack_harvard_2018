a8496dddf62ee73b504e0938ad362b456d648ef0
Kevin Tang <kevtang@mit.edu>
2018-10-21 07:14:44 -0400

    Updating ChromeExtension...

--- ChromeExtension/Musicboi Chrome Extension/popup.js
+++ ChromeExtension/Musicboi Chrome Extension/popup.js
@@ -2,27 +2,37 @@ $(function(){
 	$('#sendString').click(function(){
 		chrome.storage.sync.get('storedString', function(textToSend){
 			var stringInput = $('#stringInput').val();
-			var returned = '';
+      if (stringInput) {
+        var returnedTone = '';
+        var returnedLink = '';
 
-			chrome.storage.sync.set({'storedString': stringInput})
+        chrome.storage.sync.set({'storedString': stringInput})
 
-			let url = 'https://musicbois.serveo.net/submitString';
-  			let input = stringInput;
+        let url = 'https://musicbois.serveo.net/submitString';
+        let input = stringInput;
 
-  			$('#stringInput').val('');
-  			$('#link').text('Generating...')
+        $('#stringInput').val('');
+        $('#tone').text('Calculating...')
+        $('#link').text('Generating...')
  
-  			fetch(url, {
-    			method: 'POST',
-    			datatype: "json",
-    			headers: {"Content-Type": "application/x-www-form-urlencoded"},
-    			body: "&text="+input
-    		})
-    		.then(res => res.json())
-    		.then((resp) => {
-    			returned = resp.text;
-    			$('#link').text(returned);
-    		})
+        fetch(url, {
+          method: 'POST',
+          datatype: "json",
+          headers: {"Content-Type": "application/x-www-form-urlencoded"},
+          body: "&text="+input
+        })
+        .then(res => res.json())
+        .then((resp) => {
+          returnedTone = resp.text;
+          returnedLink = resp.link;
+          $('#tone').text(returnedTone);
+          $('#link').text(returnedLink);
+        })
+      }
+      else {
+        $('#tone').text('Please enter a text!')
+        $('#link').text('Please enter a text!')
+      }
 		});
 	});
 });
